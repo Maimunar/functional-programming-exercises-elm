ebd42708b40feca467baf4efbe7de55defe2db73
Aleksandar Ivanov <425981@student.fontys.nl>
2021-03-05 19:51:37 +0100

    Added 2 new exercises

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Some exercises made into a single-page application on elm.
 1. Palindrome Checker - checks if an input is a palindrome or not
 2. Split a List - Splits a list into 2 lists at a specific item
 3. Remove nth element - Removes an element on a specific index from a list
+4. Run-length encoding - Splits a list of same-item lists to a list of tuples with <Item, Count>
+5. Gray Code - Error checking in digital communications. Find out more here : https://www.youtube.com/watch?v=sasnEZBBX64
--- src/Main.elm
+++ src/Main.elm
@@ -1,46 +1,202 @@
-module Main exposing (main)
+module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import String
-import List
+import List exposing (..)
 import Debug exposing (toString)
+import List.Extra
+import List
 
+-- MAIN
 
--- Change this code to try with different arrays
-palindromeCheckArray = 
-    [1,2,5,2,1]
-
--- Assignment 1
-isPalindrome: List a -> Bool
-isPalindrome listToCheck = 
-    listToCheck == (List.reverse listToCheck)
-
-
+main : Program () Model Msg
 main =
+    Browser.sandbox
+        { init = init
+        , view = view
+        , update = update
+        }
+
+
+-- MODEL
+
+type alias Model =
+    { palindromeInput : String,
+    listSplitIndex : String,
+    removeItemAtIndex : String,
+    rleInput: String,
+    grayInput: String}
+
+
+init : Model
+init =
+    { palindromeInput = "Please input here",
+    listSplitIndex = "0",
+    removeItemAtIndex = "0",
+    rleInput="",
+    grayInput=""}
+
+-- UPDATE
+
+type Msg
+    = SetPalindromeInput String
+    | SetListSplitIndex String
+    | SetRemoveItemAtIndex String
+    | SetRleInput String
+    | SetGrayInput String
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        SetPalindromeInput newInput ->
+            {model | palindromeInput = newInput}
+        SetListSplitIndex newIndex ->
+            {model | listSplitIndex = newIndex }
+        SetRemoveItemAtIndex newIndex ->
+            {model | removeItemAtIndex = newIndex}
+        SetRleInput newInput ->
+            {model | rleInput = newInput}
+        SetGrayInput newInput ->
+            {model | grayInput = newInput}
+        
+
+-- VIEW
+
+view : Model -> Html Msg
+view model = 
     div
         []
         [
             node "link"
-            [ rel "stylesheet"
-            , href "stylesheets/main.css"
-            ]
-            [],
-            div
-            [ class "container"]
-            [ 
-                h3 [class "palindrome-text"] [text "Palindrome Checker"],
-                p [class "center"] [text (String.concat ["Input - ", toString palindromeCheckArray])],
-                p [class "center"] [text (String.concat ["Output - ", toString (isPalindrome palindromeCheckArray)])]
-            ],
+                [ rel "stylesheet"
+                , href "stylesheets/main.css"
+                ]
+                [],
             div
             [ class "container"]
             [
                 h3 [class "palindrome-text"] [text "Palindrome Checker"],
-                p [class "center"] [text (String.concat ["Input - ", toString palindromeCheckArray])],
-                p [class "center"] [text (String.concat ["Output - ", toString (isPalindrome palindromeCheckArray)])]
-            ]            
+                p [class "center"] [text (String.concat ["Input - ", model.palindromeInput])],
+                input [ type_ "text", class "center", onInput SetPalindromeInput] [],
+                p [class "center"] [text (String.concat ["Output - ", toString (isPalindrome (String.toList model.palindromeInput))])]
+            ],
+            div
+            [ class "container"]
+            [
+                h3 [class "palindrome-text"] [text "List Split"],
+                p [class "center"] [text (String.concat ["List - ", toString exampleList])],
+                p [class "center"] [text (String.concat ["Split at that much numbers - ", model.listSplitIndex])],
+                input [ type_ "number", class "center", onInput SetListSplitIndex] [],
+                p [class "center"] [text (String.concat ["Output - ", (toString (listSplit exampleList (String.toInt model.listSplitIndex |> Maybe.withDefault 0)))])]
+            ],
+            div
+            [ class "container"]
+            [
+                h3 [class "palindrome-text"] [text "Remove Element at N"],
+                p [class "center"] [text (String.concat ["List - ", toString exampleList])],
+                p [class "center"] [text (String.concat ["Remove Item at - ", model.removeItemAtIndex])],
+                input [ type_ "number", class "center", onInput SetRemoveItemAtIndex] [],
+                p [class "center"] [text (String.concat ["Output - ", (toString (removeAtN exampleList (String.toInt model.removeItemAtIndex |> Maybe.withDefault 0)))])]
+            ],
+            div
+            [ class "container"]
+            [
+                h3 [class "palindrome-text"] [text "Run-Length-Encoding"],
+                p [class "center"] [text (String.concat ["Input - ", 
+                toString (String.toList model.rleInput )])],
+                input [ type_ "text", class "center", onInput SetRleInput] [],                                                             
+                p [class "center"] [text (String.concat ["Output - ", 
+                toString (toListofEquals (String.toList model.rleInput))])]
+            ],
+            div
+            [ class "container"]
+            [
+                h3 [class "palindrome-text"] [text "Gray Code"],
+                p [class "center"] [text (String.concat ["Input - ", 
+                model.grayInput])],
+                input [ type_ "text", class "center", onInput SetGrayInput] [],                                                             
+                p [class "center"] [text (String.concat ["Output - ", 
+                toString (calculateGray (String.toInt model.grayInput |> Maybe.withDefault 0))])]
+            ]
         ]
     
+-- Assignment 1
+-- If a list and its reverse are equal, the list is a palindrome
+isPalindrome: List a -> Bool
+isPalindrome listToCheck = 
+    listToCheck == (List.reverse listToCheck)
+
+-- Assignment 2
+-- An example list im using for the exercises
+exampleList = 
+    [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+-- My approach is to create a list of the 2 separated lists
+listSplit: List a -> Int -> List (List a)
+listSplit inputList indexToSplit =
+    [(take indexToSplit inputList), (drop indexToSplit inputList)]
+
+-- Assignment 3
+-- Create a list of all elements, except take 1 less at the index to remove at n
+removeAtN: List a -> Int -> List a
+removeAtN inputList indexToRemove =
+    (take (indexToRemove - 1) inputList) ++ (drop indexToRemove inputList)
+
+-- Assignment 4
+-- Create a list of tuples (Item, Count) from a list of single-item lists
+
+
+-- METHOD NOT IN USE
+-- Frontend helper function to convert the strings to a list of lists
+-- Also filters out empty lists
+-- Currently using the method bellow (toListofEquals)
+toListofLists: String -> String -> String -> String -> String -> List (List Char)
+toListofLists a b c d e =
+    List.filter (\x -> List.length x > 0) [(String.toList a), (String.toList b), (String.toList c), (String.toList d), (String.toList e)]
+-- Actual assignment code
+-- Takes a list of lists of chars - [['a','a'], ['b','b']]
+-- Returns a list of tuples - [('a',2), ('b',2)]
+-- The Maybe.withDefault is used to turn a maybe Char into a definite char (needed for my type checking)
+runLengthEncoding: List (List Char) -> List (Char, Int)
+runLengthEncoding listToConvert =
+    (List.map (\x -> (head x|> Maybe.withDefault '0', List.length x)) listToConvert)
+
+
+-- Better code for Assignment 4
+-- Takes as much inputs as you would like to
+toListofEquals : List Char-> List (Char, Int)
+toListofEquals input =
+    List.map 
+    convertTuple
+    (List.Extra.group input)
+
+-- List.Extra.group gives the values as ['1','1','1'] -> ('1', ['1','1'])
+-- This helper function goes in the map function and does the following:
+-- ('1', ['1','1']) -> ('1', 3)
+convertTuple: (a, List a) -> (a, Int)
+convertTuple tuple = 
+    (Tuple.first tuple, (List.length (Tuple.second tuple) + 1))
+
+
+-- Assignment 5
+-- Recursively calculating gray's code
+calculateGray: Int -> List String
+calculateGray n =
+    -- Edge case, mostly for errors and the start case
+    if (n <= 0) then
+        ["0"]
+    -- Base case, that's how you get the first numbers
+    else if (n == 1) then
+        ["0", "1"]
+    else
+        let
+            -- Following the recursion structure guide on elm's website
+            recursionGray = calculateGray (n-1)
+        in
+            -- First adding all available items with a 0 in front
+            (List.map (\x -> "0" ++ x) recursionGray) ++ 
+            -- Secondly adding them reversed with a 1 in front
+            (List.map (\x -> "1" ++ x) (List.reverse recursionGray))
